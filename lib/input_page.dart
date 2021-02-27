a30b48c73cd5b7bad214e40840904f32e429eb33
Al-AizHashim <65592342+Al-AizHashim@users.noreply.github.com>
2021-02-27 22:26:21 +0300

    Project files are orgnized

---
+++ lib/input_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  InputPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BMI CALCULATOR"),
+      ),
+      body: Center(
+        child: Text(
+          'Body text',
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,21 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'input_page.dart';
 
 void main() {
   runApp(BIMCalculator());
 }
 
 class BIMCalculator extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: InputPage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  InputPage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("BMI CALCULATOR"),
-      ),
-      body: Center(
-        child: Text(
-          'Body text',
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
+      theme: ThemeData.dark().copyWith(
+          primaryColor: Color(0xFF0A0e21),
+          scaffoldBackgroundColor: Color(0xFF0A0e21)),
+      debugShowCheckedModeBanner: false,
+      home: InputPage(),
     );
   }
 }
